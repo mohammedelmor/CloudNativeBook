1966597519493b2daed066b6bb9e56ccf9683ce4
MoAtef <mohammedelmor7@gmail.com>
2024-02-14 22:28:56 +0400

    Adding Config Client

---
+++ src/main/java/com/polarbookshop/catalogservice/HomeConroller.java
@@ -0,0 +1,21 @@
+package com.polarbookshop.catalogservice;
+
+import com.polarbookshop.catalogservice.config.PolarProperties;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeConroller {
+
+    private final PolarProperties polarProperties;
+
+    public HomeConroller(PolarProperties polarProperties) {
+        this.polarProperties = polarProperties;
+    }
+
+    @GetMapping("/")
+    public String getGreeting() {
+        return polarProperties.getGreeting();
+    }
+
+}
